a66b5d9eaf1d661bda6e65ea8927184f1b1bf957
GraceIR <140179078+GraceIR@users.noreply.github.com>
2023-10-17 12:46:24 +0100

    Guess game

---
+++ app.py
@@ -0,0 +1,11 @@
+Secert_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int (input('Guess:'))
+    guess_count += 1 
+if guess == Secert_number:
+    print('You Won!')
+else:
+ print('Sorry,you failed!')
+ 
